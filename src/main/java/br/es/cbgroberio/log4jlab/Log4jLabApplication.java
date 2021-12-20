ca05afa71a58ad616b57f33fa89ff38ecf84a882
Camilo Groberio <camilo.groberio@gmail.com>
2021-12-20 19:47:54 -0300

    1st commit - initial structure

---
+++ src/main/java/br/es/cbgroberio/log4jlab/Log4jLabApplication.java
@@ -0,0 +1,13 @@
+package br.es.cbgroberio.log4jlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Log4jLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Log4jLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/es/cbgroberio/log4jlab/Log4jLabApplicationTests.java
@@ -0,0 +1,13 @@
+package br.es.cbgroberio.log4jlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Log4jLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
